5bfcedcac699cc8bca40363d7994cec80307c01d
steven-rj <stevenrjfiles@gmail.com>
2021-02-09 21:26:27 -0500

    added chapters, refined formatting

--- js/quotes.js
+++ js/quotes.js
@@ -3,23 +3,48 @@
 ***/
 
 const quotes = [
-    {quote: `The Tao that can be understood is not the eternal, cosmic Tao, just as an idea that can be expressed in words is not the infinite idea.
-    <br><br>And yet this ineffable Tao is the source of all spirit and matter;
-    expressing itself
-    it is the mother of all created things.<br><br>
-  Not to desire material things
-    is to know the freedom of spirituality;
-    and to desire them is to suffer
-    the limitations of matter.<br><br>
-  Yet these two things, matter and spirit,
-    so different in nature, have the same origin.<br><br>
-  This unity is the mystery of mysteries,
-    and the gateway to spirituality.`,
+    {quote: `The Tao that can be understood is not the eternal, cosmic Tao, just as an idea that can be expressed in words is not the infinite idea.<br><br>
+        And yet this ineffable Tao is the source of all spirit and matter;
+        expressing itself
+        it is the mother of all created things.<br><br>
+      Not to desire material things
+        is to know the freedom of spirituality;
+        and to desire them is to suffer
+        the limitations of matter.<br><br>
+      Yet these two things, matter and spirit,
+        so different in nature, have the same origin.<br><br>
+      This unity is the mystery of mysteries,
+        and the gateway to spirituality.`,
     source: "Lao Tzu",
     citation: "What is the Tao?",
     //year: 201,
     //actor: "What is the Tao?",
     photo: "images/"},
 
+
+    {quote: `When beauty is only a masquerade,
+        it is actually ugliness.
+        And goodness, if it is not sincere,
+        is not really goodness.<br><br>
+      Existence and nonexistence are incompatible.
+      The difficult and the easy are mutually opposed.
+      The long and the short, the high and the low,
+      the loud and soft, the before and after--
+      all are opposites.
+      Each reveals the other.<br><br>
+      The wise are not conspicuous in their actions
+        or given to much talking.
+        When troubles arise,
+        they are not irritated.<br><br>
+      They produce, but do not hoard,
+        They act, but expect no praise,
+        They build, but do not dwell therein.<br><br>
+      And because they do not dwell therein,
+        they never depart.`,
+    source: "Lao Tzu",
+    citation: "Opposites",
+    //year: 201,
+    //actor: "What is the Tao?",
+    photo: "images/"},
 
 ]
