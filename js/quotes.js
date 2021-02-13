351d6f9f74ab87b75fe8573b22e568a7fd191987
steven-rj <stevenrjfiles@gmail.com>
2021-02-13 14:33:05 -0500

    edited in-code quotation formatting

--- js/quotes.js
+++ js/quotes.js
@@ -3,28 +3,31 @@
 ***/
 
 const quotes = [
-    {quote: `The Tao that can be understood is not the eternal, cosmic Tao, just as an idea that can be expressed in words is not the infinite idea.<br><br>
-        And yet this ineffable Tao is the source of all spirit and matter;
-        expressing itself
-        it is the mother of all created things.<br><br>
+    {quote:
+      `The Tao that can be understood is not the eternal, cosmic Tao,
+      just as an idea that can be expressed in words is not the infinite idea.<br><br>
+      And yet this ineffable Tao is the source of all spirit and matter;
+      expressing itself
+      it is the mother of all created things.<br><br>
       Not to desire material things
-        is to know the freedom of spirituality;
-        and to desire them is to suffer
-        the limitations of matter.<br><br>
+      is to know the freedom of spirituality;
+      and to desire them is to suffer
+      the limitations of matter.<br><br>
       Yet these two things, matter and spirit,
-        so different in nature, have the same origin.<br><br>
+      so different in nature, have the same origin.<br><br>
       This unity is the mystery of mysteries,
-        and the gateway to spirituality.`,
+      and the gateway to spirituality.`,
     source: "Lao Tzu",
     citation: "What is the Tao?",
     //year: 201,
     //actor: "What is the Tao?",
     photo: "images/"},
 
-    {quote: `When beauty is only a masquerade,
-        it is actually ugliness.
-        And goodness, if it is not sincere,
-        is not really goodness.<br><br>
+    {quote:
+      `When beauty is only a masquerade,
+      it is actually ugliness.
+      And goodness, if it is not sincere,
+      is not really goodness.<br><br>
       Existence and nonexistence are incompatible.
       The difficult and the easy are mutually opposed.
       The long and the short, the high and the low,
@@ -32,32 +35,51 @@ const quotes = [
       all are opposites.
       Each reveals the other.<br><br>
       The wise are not conspicuous in their actions
-        or given to much talking.
-        When troubles arise,
-        they are not irritated.<br><br>
+      or given to much talking.
+      When troubles arise,
+      they are not irritated.<br><br>
       They produce, but do not hoard,
-        They act, but expect no praise,
-        They build, but do not dwell therein.<br><br>
+      They act, but expect no praise,
+      They build, but do not dwell therein.<br><br>
       And because they do not dwell therein,
-        they never depart.`,
+      they never depart.`,
     source: "Lao Tzu",
     citation: "Opposites",
     //year: 201,
     //actor: "What is the Tao?",
     photo: "images/"},
 
-    {quote: `When a ruler is silent on the subject of virtue,
-    the people are discouraged from practicing it.
-    Meanwhile, a ruler who revels in riches encourages thievery.<br><br>
-    Value virtue over wealth,
-    and the people's hearts will be at rest.<br><br>
-    Wise rulers do not accumulate treasures,
-    but seek to quiet the hearts of their people.
-    They soothe the people's appetites and strengthen their bones.
-    They treasure innocence, and protect the simple from the schemes of the clever.<br><br>
-    When a ruler practices restraint, everything will be in place.`,
+    {quote:
+      `When a ruler is silent on the subject of virtue,
+      the people are discouraged from practicing it.
+      Meanwhile, a ruler who revels in riches encourages thievery.<br><br>
+      Value virtue over wealth,
+      and the people's hearts will be at rest.<br><br>
+      Wise rulers do not accumulate treasures,
+      but seek to quiet the hearts of their people.
+      They soothe the people's appetites and strengthen their bones.
+      They treasure innocence, and protect the simple from the schemes of the clever.<br><br>
+      When a ruler practices restraint, everything will be in place.`,
     source: "Lao Tzu",
     citation: "Restraint",
+    //year: 201,
+    //actor: "What is the Tao?",
+    photo: "images/"},
+
+    {quote:
+      `The Tao seems empty, yet it is never exhausted.<br><br>
+      Oh, it is profound!
+      It existed before anything.<br><br>
+      It dulls its own sharpness,
+      breaks its own bonds,
+      dims its own brightness,
+      identifies with its own dust.<br><br>
+      Oh, it is peaceful!
+      It is infinite, eternal.<br><br>
+      No one knows from where it came.
+      It is older than the gods.<br>`,
+    source: "Lao Tzu",
+    citation: "The Eternal Tao",
     //year: 201,
     //actor: "What is the Tao?",
     photo: "images/"},
