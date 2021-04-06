bc41b589f523b86c5e9f5d66e31e124f02c53f28
steven-rj <stevenrjfiles@gmail.com>
2021-04-06 18:06:05 -0400

    added blank quote templates

--- js/quotes.js
+++ js/quotes.js
@@ -1077,4 +1077,28 @@ const quotes = [
     //actor: "What is the Tao?",
     photo: "images/"},
 
+    {quote:
+      ``,
+    source: "Lao Tzu",
+    citation: "",
+    //year: 201,
+    //actor: "What is the Tao?",
+    photo: "images/"},
+
+    {quote:
+      ``,
+    source: "Lao Tzu",
+    citation: "",
+    //year: 201,
+    //actor: "What is the Tao?",
+    photo: "images/"},
+
+    {quote:
+      ``,
+    source: "Lao Tzu",
+    citation: "",
+    //year: 201,
+    //actor: "What is the Tao?",
+    photo: "images/"},
+
 ]
